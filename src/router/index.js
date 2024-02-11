74cd7ae86c337ca61761574af9308b9766c12324
Jesús Toledo <42069286+jtoledod@users.noreply.github.com>
2024-02-11 12:11:43 -0400

    feat: add a not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/history',
       name: 'history',
       component: () => import('@/views/HistoryView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: NotFoundView
     }
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main
+    class="item-center container mt-[57.6px] flex min-h-[calc(100vh-153.6px)] flex-col justify-center text-center"
+  >
+    <h1 class="mb-4 text-3xl font-bold tracking-tight">Página no encontrada</h1>
+    <RouterLink :to="{ name: 'home' }" class="text-md hover:text-indigo-500"
+      >Volver al inicio</RouterLink
+    >
+  </main>
+</template>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
